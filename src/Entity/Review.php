bca0fdfe77f9a047976bd166a0758f8e4cb9ce23
Vitoria Costa Ferreira <ferreiracvitoria@outlook.com>
2023-09-26 13:28:42 +0200

    Creating entity

---
+++ src/Entity/Review.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReviewRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReviewRepository::class)]
+class Review
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $commentaire = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateReview = null;
+
+    #[ORM\Column]
+    private ?int $rating = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(string $commentaire): static
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    public function getDateReview(): ?\DateTimeInterface
+    {
+        return $this->dateReview;
+    }
+
+    public function setDateReview(\DateTimeInterface $dateReview): static
+    {
+        $this->dateReview = $dateReview;
+
+        return $this;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(int $rating): static
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+}
